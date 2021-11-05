bd7f96327be645c165872160300b85b0d79e3e51
Sebastian Lewandowski <hulio90@gmail.com>
2021-11-05 22:36:02 +0100

    more comments, some cosmetic changes

--- lab2/main.py
+++ lab2/main.py
@@ -21,7 +21,7 @@ wyjściu otrzymujemy ocenę.
     -`cena`
  	    * ocena uniwersalna( wartości 0-10)
 	    * ocena dla logiki rozmytej( poor, average, good)
-    -`lokalizacja`
+    - `lokalizacja`
         * ocena uniwersalna( wartości 0-10)
         * ocena dla logiki rozmytej( poor, average, good)
     -`dodatki`
@@ -31,8 +31,8 @@ wyjściu otrzymujemy ocenę.
     - `ocena`
         * ocena dla logiki rozmytej( verylow, low, medium, high, veryhigh)
 * Zasady
-    - Jeśli cena lub lokalizacja są dobre to ocena będzie „high”
-    - Jeśli cena jest „average”, to ocena będzie „medium”
+    - Jeśli cena lub lokalizacja są "good" to ocena będzie „high”
+    - Jeśli cena lub lokalizacja jest „average”, to ocena będzie „medium”
     - Jeśli cena będzie „poor” i lokalizacja będzie „poor” to ocena będzie „low”
     - Jeśli cena, dodatki i lokalizacja będzie „poor” to ocena będzie „verylow”
 
@@ -44,7 +44,8 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
-#deklarujemy potrzebne zmienne dla logiki rozmytej
+# deklarujemy potrzebne zmienne dla logiki rozmytej
+# 3 na wejsciu, jedna na wyjściu
 localization = ctrl.Antecedent(np.arange(0, 11, 1), 'localization')
 price = ctrl.Antecedent(np.arange(0, 11, 1), 'price')
 additions = ctrl.Antecedent(np.arange(0, 11, 1), 'additions')
@@ -53,53 +54,46 @@ rating = ctrl.Consequent(np.arange(0, 11, 1), 'rating')
 #
 localization.automf(3)
 price.automf(3)
-additions.automf(7)
+additions.automf(5)
 
-#
+# przyjmujemy 5 wartosci lignwistycznych dla oceny: verylow, low, miedium, high,
+# veryhigh
 rating['verylow'] = fuzz.trimf(rating.universe, [0, 2, 2])
 rating['low'] = fuzz.trimf(rating.universe, [2, 4, 4])
 rating['medium'] = fuzz.trimf(rating.universe, [4, 6, 6])
 rating['high'] = fuzz.trimf(rating.universe, [6, 8, 8])
 rating['veryhigh'] = fuzz.trimf(rating.universe, [8, 10, 10])
 
-#
+# wyswietlamy zasady dla lokalizacji, ceny i oceny za pomocą wykresów
 localization['average'].view()
-"""
-.. image:: PLOT2RST.current_figure
-"""
 price.view()
-"""
-.. image:: PLOT2RST.current_figure
-"""
 rating.view()
-"""
-.. image:: PLOT2RST.current_figure
-"""
 
-#
+
+# deklarujemy nasze zasady
 rule1 = ctrl.Rule(localization['poor'] | price['poor'] | additions['poor'], rating['verylow'])
 rule2 = ctrl.Rule(price['average'], rating['medium'])
 rule3 = ctrl.Rule(price['good'] | localization['good'], rating['high'])
 rule4 = ctrl.Rule(localization['average'] | price['poor'], rating['medium'])
 rule5 = ctrl.Rule(localization['poor'] | price['poor'], rating['low'])
 
-#
+# ControlSystem jest to klasa bazowa zawierajaca nasze zasady
 ratinging_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5])
 
 #
 ratinging = ctrl.ControlSystemSimulation(ratinging_ctrl)
 
-#
+# tutaj podajemy nasze wartości wejściowe
 ratinging.input['localization'] = 5.7
 ratinging.input['price'] = 7.8
 ratinging.input['additions'] = 6.2
 
-#
+# obliczamy rating
 ratinging.compute()
 
 #
 print (ratinging.output['rating'])
 rating.view(sim=ratinging)
 
-#
+# wyswietlamy wykres za pomocą matplotlib
 plt.show() 
