4b8f1fbffdfdfab56c6768ad106eabda9e78bb07
DESKTOP-KQUR29E\Tomek <tplasticsamol@gmail.com>
2021-11-06 09:44:36 +0100

    added rules & comments

--- lab2/main.py
+++ lab2/main.py
@@ -31,10 +31,34 @@ wyjściu otrzymujemy ocenę.
     - `ocena`
         * ocena dla logiki rozmytej( verylow, low, medium, high, veryhigh)
 * Zasady
-    - Jeśli cena lub lokalizacja są "good" to ocena będzie „high”
-    - Jeśli cena lub lokalizacja jest „average”, to ocena będzie „medium”
-    - Jeśli cena będzie „poor” i lokalizacja będzie „poor” to ocena będzie „low”
-    - Jeśli cena, dodatki i lokalizacja będzie „poor” to ocena będzie „verylow”
+    - Jeśli cena to  dodatki i lokalizacja będzie „poor” to ocena będzie „verylow”
+    Jeśli lokalizacja będzie "poor" & cena będzie "poor" & dodatki będą "poor" to  rating będzie "verylow")
+    Jeśli lokalizacja będzie "poor" & cena będzie "average" & dodatki będą "poor" to  rating będzie "verylow")
+    Jeśli lokalizacja będzie "poor" & cena będzie "poor" & dodatki będą "average" to  rating będzie "verylow")
+    Jeśli lokalizacja będzie "poor" & cena będzie "average" & dodatki będą "average" to  rating będzie "low")
+    Jeśli lokalizacja będzie "poor" & cena będzie "good" & dodatki będą "average" to  rating będzie "medium")
+    Jeśli lokalizacja będzie "poor" & cena będzie "average" & dodatki będą "good" to  rating będzie "medium")
+    Jeśli lokalizacja będzie "poor" & cena będzie "good" & dodatki będą "good" to  rating będzie "medium")
+    Jeśli lokalizacja będzie "poor" & cena będzie "poor" & dodatki będą "good" to  rating będzie "low")
+    Jeśli lokalizacja będzie "poor" & cena będzie "good" & dodatki będą "poor" to  rating będzie "low")
+    Jeśli lokalizacja będzie "average" & cena będzie "poor" & dodatki będą "poor" to  rating będzie "verylow")
+    Jeśli lokalizacja będzie "average" & cena będzie "average" & dodatki będą "poor" to  rating będzie "low")
+    Jeśli lokalizacja będzie "average" & cena będzie "poor" & dodatki będą "average" to  rating będzie "low")
+    Jeśli lokalizacja będzie "average" & cena będzie "average" & dodatki będą "average" to  rating będzie "medium")
+    Jeśli lokalizacja będzie "average" & cena będzie "good" & dodatki będą "average" to  rating będzie "high")
+    Jeśli lokalizacja będzie "average" & cena będzie "average" & dodatki będą "good" to  rating będzie "high")
+    Jeśli lokalizacja będzie "average" & cena będzie "good" & dodatki będą "good" to  rating będzie "veryhigh")
+    Jeśli lokalizacja będzie "average" & cena będzie "poor" & dodatki będą "good" to  rating będzie "medium")
+    Jeśli lokalizacja będzie "average" & cena będzie "good" & dodatki będą "poor" to  rating będzie "medium")
+    Jeśli lokalizacja będzie "good" & cena będzie "poor" & dodatki będą "poor" to  rating będzie "low")
+    Jeśli lokalizacja będzie "good" & cena będzie "average" & dodatki będą "poor" to  rating będzie "medium")
+    Jeśli lokalizacja będzie "good" & cena będzie "poor" & dodatki będą "average" to  rating będzie "medium")
+    Jeśli lokalizacja będzie "good" & cena będzie "average" & dodatki będą "average" to  rating będzie "high")
+    Jeśli lokalizacja będzie "good" & cena będzie "good" & dodatki będą "average" to  rating będzie "veryhigh")
+    Jeśli lokalizacja będzie "good" & cena będzie "average" & dodatki będą "good" to  rating będzie "veryhigh")
+    Jeśli lokalizacja będzie "good" & cena będzie "good" & dodatki będą "good" to  rating będzie "medium")
+    Jeśli lokalizacja będzie "good" & cena będzie "poor" & dodatki będą "good" to  rating będzie "medium")
+    Jeśli lokalizacja będzie "good" & cena będzie "good" & dodatki będą "poor" to  rating będzie "medium")
 
 """
 
@@ -54,7 +78,7 @@ rating = ctrl.Consequent(np.arange(0, 11, 1), 'rating')
 #
 localization.automf(3)
 price.automf(3)
-additions.automf(5)
+additions.automf(3)
 
 # przyjmujemy 5 wartosci lignwistycznych dla oceny: verylow, low, miedium, high,
 # veryhigh
@@ -69,18 +93,43 @@ localization['average'].view()
 price.view()
 rating.view()
 
-
 # deklarujemy nasze zasady
-rule1 = ctrl.Rule(localization['poor'] | price['poor'] | additions['poor'], rating['verylow'])
-rule2 = ctrl.Rule(price['average'], rating['medium'])
-rule3 = ctrl.Rule(price['good'] | localization['good'], rating['high'])
-rule4 = ctrl.Rule(localization['average'] | price['poor'], rating['medium'])
-rule5 = ctrl.Rule(localization['poor'] | price['poor'], rating['low'])
+rule1 = ctrl.Rule(localization['poor'] & price['poor'] & additions['poor'], rating['verylow'])
+rule2 = ctrl.Rule(localization['poor'] & price['average'] & additions['poor'], rating['verylow'])
+rule3 = ctrl.Rule(localization['poor'] & price['poor'] & additions['average'], rating['verylow'])
+rule4 = ctrl.Rule(localization['poor'] & price['average'] & additions['average'], rating['low'])
+rule5 = ctrl.Rule(localization['poor'] & price['good'] & additions['average'], rating['medium'])
+rule6 = ctrl.Rule(localization['poor'] & price['average'] & additions['good'], rating['medium'])
+rule7 = ctrl.Rule(localization['poor'] & price['good'] & additions['good'], rating['medium'])
+rule8 = ctrl.Rule(localization['poor'] & price['poor'] & additions['good'], rating['low'])
+rule9 = ctrl.Rule(localization['poor'] & price['good'] & additions['poor'], rating['low'])
+
+rule10 = ctrl.Rule(localization['average'] & price['poor'] & additions['poor'], rating['verylow'])
+rule11 = ctrl.Rule(localization['average'] & price['average'] & additions['poor'], rating['low'])
+rule12 = ctrl.Rule(localization['average'] & price['poor'] & additions['average'], rating['low'])
+rule13 = ctrl.Rule(localization['average'] & price['average'] & additions['average'], rating['medium'])
+rule14 = ctrl.Rule(localization['average'] & price['good'] & additions['average'], rating['high'])
+rule15 = ctrl.Rule(localization['average'] & price['average'] & additions['good'], rating['high'])
+rule16 = ctrl.Rule(localization['average'] & price['good'] & additions['good'], rating['veryhigh'])
+rule17 = ctrl.Rule(localization['average'] & price['poor'] & additions['good'], rating['medium'])
+rule18 = ctrl.Rule(localization['average'] & price['good'] & additions['poor'], rating['medium'])
+
+rule19 = ctrl.Rule(localization['good'] & price['poor'] & additions['poor'], rating['low'])
+rule20 = ctrl.Rule(localization['good'] & price['average'] & additions['poor'], rating['medium'])
+rule21 = ctrl.Rule(localization['good'] & price['poor'] & additions['average'], rating['medium'])
+rule22 = ctrl.Rule(localization['good'] & price['average'] & additions['average'], rating['high'])
+rule23 = ctrl.Rule(localization['good'] & price['good'] & additions['average'], rating['veryhigh'])
+rule24 = ctrl.Rule(localization['good'] & price['average'] & additions['good'], rating['veryhigh'])
+rule25 = ctrl.Rule(localization['good'] & price['good'] & additions['good'], rating['medium'])
+rule26 = ctrl.Rule(localization['good'] & price['poor'] & additions['good'], rating['medium'])
+rule27 = ctrl.Rule(localization['good'] & price['good'] & additions['poor'], rating['medium'])
 
 # ControlSystem jest to klasa bazowa zawierajaca nasze zasady
-ratinging_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5])
+ratinging_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10,
+                                     rule11, rule12, rule13, rule14, rule15, rule16, rule17, rule18, rule19, rule20,
+                                     rule21, rule22, rule23, rule24, rule25, rule26, rule27])
 
-#
+# ControlSystemSimulation tworzy kontroler na podstawie naszych zasad
 ratinging = ctrl.ControlSystemSimulation(ratinging_ctrl)
 
 # tutaj podajemy nasze wartości wejściowe
@@ -91,9 +140,9 @@ ratinging.input['additions'] = 6.2
 # obliczamy rating
 ratinging.compute()
 
-#
-print (ratinging.output['rating'])
+# wyświetlenie wyniku ratingu
+print(ratinging.output['rating'])
 rating.view(sim=ratinging)
 
 # wyswietlamy wykres za pomocą matplotlib
-plt.show() 
+plt.show()
