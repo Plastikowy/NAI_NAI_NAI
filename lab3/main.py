5ed529bc41ed9ffd2eceec5bea6c8b9053c15da6
Sebastian Lewandowski <hulio90@gmail.com>
2021-11-18 14:31:05 +0100

    created functions to find similar ppl and compute person

--- lab3/main.py
+++ lab3/main.py
@@ -8,6 +8,13 @@ def input_argument_parser():
         help='input name as argument to execute program')
     return input_parser
 
+def similar_ppl_finder(dataset, name, num_ppl):
+    if name not in dataset:
+        raise TypeError('Cannot find ' + name + ' in the dataset')
+
+    if num_ppl < 1:
+        raise TypeError('Amount of people must be equal or greater than 1')
+
 if __name__ == '__main__':
     args = input_argument_parser().parse_args()
     input_name = args.name
@@ -19,4 +26,8 @@ rating_file = 'data.json'
 with open(rating_file, mode='r') as file:
     data = json.loads(file.read())
 
-print(input_name + " movies rating: \n",data[input_name])
+print(input_name + ' movies rating: \n',data[input_name])
+
+print('\nUsers similar to ' + input_name + ':\n')
+
+#similar_users = similar_ppl_finder(rating_file, input_name, 3)
