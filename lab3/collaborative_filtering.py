d98d5b72c4580f47bf39120fd45896b50db6cac2
Sebastian Lewandowski <hulio90@gmail.com>
2021-11-18 23:32:09 +0100

    fixed code formating and delete redundant brackets

--- lab3/collaborative_filtering.py
+++ lab3/collaborative_filtering.py
@@ -4,11 +4,13 @@ import numpy as np
 
 from compute_scores import euclidean_score
 
+
 def build_arg_parser():
     parser = argparse.ArgumentParser(description='Find users who are similar to the input user')
     parser.add_argument('--user', dest='user', required=True,
-            help='Input user')
+                        help='Input user')
     return parser
+
 
 # Finds users in the dataset that are similar to the input user
 def find_similar_users(dataset, user, num_users):
@@ -18,7 +20,7 @@ def find_similar_users(dataset, user, num_users):
     # Compute Pearson score between input user
     # and all the users in the dataset
     scores = np.array([[x, euclidean_score(dataset, user,
-            x)] for x in dataset if x != user])
+                                           x)] for x in dataset if x != user])
 
     # Sort the scores in decreasing order
     scores_sorted = np.argsort(scores[:, 1])[::-1]
@@ -28,68 +30,74 @@ def find_similar_users(dataset, user, num_users):
 
     return scores[top_users]
 
+
 def add_similar_users_to_list(database, similars):
-    #saves similar user's names to list
+    # saves similar user's names to list
     for item in database:
         for sim_user in similars:
-            if(item == sim_user[0]):
+            if item == sim_user[0]:
                 similars_users_names.append(item)
 
+
 def create_films_set():
-    #add films from each similar user
+    # add films from each similar user
     for name in similars_users_names:
         # print(data[name])
         savefilms(data[name])
     # print(films)
 
-    #we need to remove lecturer seen movies
+    # we need to remove lecturer seen movies
     for paul_film in data[user]:
         films.remove(paul_film)
     # print(films)
 
+
 def find_recommended_films():
-    #initialize variables for describing recommended films
+    # initialize variables for describing recommended films
     score_index = 10
 
-    #going from the top rating, we are looping through all the films
-    #we check if users watched shared films and if so, we check their ratings,
-    #if it's current searched top rating, we add it to recommended films
+    # going from the top rating, we are looping through all the films
+    # we check if users watched shared films and if so, we check their ratings,
+    # if it's current searched top rating, we add it to recommended films
     while score_index > 0:
         for film in films:
             for name in similars_users_names:
-                  if (len(recommended_films) < 5):
-                      if (film in data[name] and data[name][film] == score_index):
-                          # print(film, 'equals', data[name][film])
-                          recommended_films.add(film)
-        score_index-=1
+                if len(recommended_films) < 5:
+                    if film in data[name] and data[name][film] == score_index:
+                        # print(film, 'equals', data[name][film])
+                        recommended_films.add(film)
+        score_index -= 1
+
 
 def find_not_recommended_films():
-    #initialize variables for describing recommended films
+    # initialize variables for describing recommended films
     score_index = 1
 
-    #going from the top rating, we are looping through all the films
-    #we check if users watched shared films and if so, we check their ratings,
-    #if it's current searched top rating, we add it to recommended films
+    # going from the top rating, we are looping through all the films
+    # we check if users watched shared films and if so, we check their ratings,
+    # if it's current searched top rating, we add it to recommended films
     while score_index < 11:
         for film in films:
             for name in similars_users_names:
-                  if (len(not_recommended_films) < 5):
-                      if (film in data[name] and data[name][film] == score_index):
-                          # print(film, 'equals', data[name][film])
-                          not_recommended_films.add(film)
-        score_index+=1
+                if len(not_recommended_films) < 5:
+                    if film in data[name] and data[name][film] == score_index:
+                        # print(film, 'equals', data[name][film])
+                        not_recommended_films.add(film)
+        score_index += 1
 
-#function to save films to set
+
+# function to save films to set
 def savefilms(user_data):
     for film in user_data:
         films.add(film)
         # print(film)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     recommended_films = set()
     not_recommended_films = set()
     similars_users_names = []
-    films = set() #we dont want repeated films
+    films = set()  # we dont want repeated films
     args = build_arg_parser().parse_args()
     user = args.user
 
@@ -103,9 +111,8 @@ if __name__=='__main__':
     print('\nUsers similar to ' + user + ':\n')
     similar_users = find_similar_users(data, user, 3)
 
-
     print('User\t\t\tSimilarity score')
-    print('-'*41)
+    print('-' * 41)
     for item in similar_users:
         print(item[0], '\t\t', round(float(item[1]), 2))
 
@@ -116,4 +123,3 @@ if __name__=='__main__':
 
     print('\n\nRECOMMENDED FILMS: ', recommended_films)
     print('NOT RECOMMENDED FILMS: ', not_recommended_films)
-
