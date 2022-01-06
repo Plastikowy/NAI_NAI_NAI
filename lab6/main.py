b8d94037a8b2ea057880084d03da6f300a4e6d71
DESKTOP-KQUR29E\Tomek <tplasticsamol@gmail.com>
2022-01-06 02:39:02 +0100

    after 4hours brainstorm, addition of one line (which renders image) solves the problem, hooray!

--- lab6/main.py
+++ lab6/main.py
@@ -18,48 +18,55 @@ import cv2
 import numpy as np
 import mediapipe as mp
 
-#TODO:
-#capture video
-#recognize face
-#recognize hand
-#outline face (red)
-#outline hand (green)
-#print crosshair on face
-#print aiming at moving target
+# TODO:
+# capture video
+# recognize face
+# recognize hand
+# outline face (red)
+# outline hand (green)
+# print crosshair on face
+# print aiming at moving target
 
-#declare video capturing variable
-cap = cv2.VideoCapture(1)
-#declare variable for mediapipe hands modules
+# declare video capturing variable
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+# declare variable for mediapipe hands modules
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 mpDrawStyles = mp.solutions.drawing_styles
 
-#def captureVideo():
+# def captureVideo():
 
 
-while(cap.isOpened()):
-    #captureVideo()
+while (cap.isOpened()):
+    # captureVideo()
     ret, frame = cap.read()
+    cv2.startWindowThread()
+    cv2.namedWindow('Camera Capture')
     cv2.imshow('Camera Capture', frame)
-    #convert colours captured from camera to RGB
+
+    # convert colours captured from camera to RGB
     imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
 
-    #print(results.multi_hand_landmarks)
+    # print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
-        print(results.multi_hand_landmarks)
+
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(
-                frame,
-                handLms,
-                mpHands.HAND_CONNECTIONS,
-                mpDrawStyles.get_default_hand_landmarks_style(),
-                mpDrawStyles.get_default_hand_connections_style())
+            print(results.multi_hand_landmarks[0])
+            mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS,
+                                  mpDrawStyles.get_default_hand_landmarks_style(),
+                                  mpDrawStyles.get_default_hand_connections_style())
 
-    readKey = cv2.waitKey(10)
-
-
+    cv2.imshow('Camera Capture', frame)
+    readKey = cv2.waitKey(1)
+    if readKey == ord('b'):
+        cv2.imwrite('screenshot_now.jpg', frame)
+        print('Zrzut zrobiony')
+    elif readKey == ord('t'):
+        cv2.circle(frame, (320,240), 30, (255,0,128), 5)
+        cv2.imwrite('screenshot_now.jpg', frame)
+        print('Rysuj kropke')
 
 cap.release()
 cv2.destroyAllWindows()
